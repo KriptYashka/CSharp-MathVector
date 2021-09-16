eac5f234019265e9383575fbb1f89d9d0e5cdee5
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2021-09-16 14:45:08 +0300

    Init

---
+++ CSharp-MathVector/MathVector.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp_LinearAlgebra
+{
+	public interface IMathVector : IEnumerable
+	{
+		/// <summary>
+		/// Получить размерность вектора (количество координат).
+		/// </summary>
+		int Dimensions { get; }
+
+		/// <summary>
+		/// Индексатор для доступа к элементам вектора. Нумерация с нуля.
+		/// </summary>
+		double this[int i] { get; set; }
+
+		/// <summary>
+		/// Рассчитать длину (модуль) вектора.
+		/// </summary>
+		double Length { get; }
+
+		/// <summary>
+		/// Покомпонентное сложение с числом.
+		/// </summary>
+		IMathVector SumNumber(double number);
+
+		/// <summary>
+		/// Покомпонентное умножение на число.
+		/// </summary>
+		IMathVector MultiplyNumber(double number);
+
+		/// <summary>
+		/// Сложение с другим вектором.
+		/// </summary>
+		IMathVector Sum(IMathVector vector);
+
+		/// <summary>
+		/// Покомпонентное умножение с другим вектором.
+		/// </summary>
+		IMathVector Multiply(IMathVector vector);
+
+		/// <summary>
+		/// Скалярное умножение на другой вектор.
+		/// </summary>
+		double ScalarMultiply(IMathVector vector);
+
+		/// <summary>
+		/// Покомпонентно поделить на другой вектор
+		/// </summary>
+		public IMathVector Divide(IMathVector vector);
+
+		public IMathVector Divide(double k);
+
+
+		/// <summary>
+		/// Вычислить Евклидово расстояние до другого вектора.
+		/// </summary>
+		/// http://kadm.kmath.ru/files/linalg17.pdf#:~:text=Расстоянием%20между%20векторами%20x%20и,с%20обычным%20скалярным%20произведением%20векторов
+		double CalcDistance(IMathVector vector);
+	}
+
+	public class MathVector : IMathVector
+	{
+		private double[] points;
+
+		public MathVector(MathVector vector)
+        {
+			this.points = vector.points;
+        }
+
+		public MathVector(double[] newPoints)
+        {
+			points = newPoints;
+        }
+
+		public int Dimensions 
+		{
+            get
+            {
+				return points.Length;
+            }
+		}
+
+		public double this[int i] 
+		{
+			get 
+			{
+				return points[i];
+			}
+			set 
+			{
+				points[i] = value;
+			}
+		}
+
+		public double Length 
+		{
+            get
+            {
+				double result = 0;
+				foreach (double point in points)
+                {
+					result += point * point;
+                }
+				result = Math.Sqrt(result);
+				return result;
+            }
+		}
+
+		public IMathVector SumNumber(double number) {
+			double[] newPoints = new double[Dimensions];
+			for (int i = 0; i < Dimensions; ++i)
+            {
+				newPoints[i] = points[i] + number;
+            }
+			return new MathVector(newPoints);
+		}
+
+		public IMathVector MultiplyNumber(double number)
+        {
+			double[] newPoints = new double[Dimensions];
+			for (int i = 0; i < Dimensions; ++i)
+			{
+				newPoints[i] = points[i] * number;
+			}
+			return new MathVector(newPoints);
+		}
+
+		public IMathVector Sum(IMathVector vector)
+        {
+			if (vector.Dimensions != this.Dimensions)
+            {
+				return null;
+			} 
+			else
+            {
+				double[] newPoints = new double[Dimensions];
+				for (int i = 0; i < Dimensions; ++i)
+				{
+					newPoints[i] = this[i] + vector[i];
+				}
+				return new MathVector(newPoints);
+			}
+		}
+
+		public IMathVector Multiply(IMathVector vector)
+        {
+			if (vector.Dimensions != this.Dimensions)
+			{
+				return null;
+			}
+			else
+			{
+				double[] newPoints = new double[Dimensions];
+				for (int i = 0; i < Dimensions; ++i)
+				{
+					newPoints[i] = vector[i] * this[i];
+				}
+				return new MathVector(newPoints);
+			}
+		}
+
+		public double ScalarMultiply(IMathVector vector)
+        {
+			if (vector.Dimensions != this.Dimensions)
+			{
+				return 0;
+			}
+			else
+			{
+				double res = 0;
+				for (int i = 0; i < Dimensions; ++i)
+				{
+					res += vector[i] * this[i];
+				}
+				return res;
+			}
+		}
+
+		public IMathVector Divide(double k)
+        {
+			double[] newPoints = new double[this.Dimensions];
+			for (int i = 0; i < this.Dimensions; ++i)
+			{
+				newPoints[i] = this[i] / k;
+			}
+			return new MathVector(newPoints);
+		}
+
+		public IMathVector Divide(IMathVector vector)
+		{
+			if (vector.Dimensions != this.Dimensions)
+			{
+				return null;
+			}
+			else
+			{
+				double[] newPoints = new double[Dimensions];
+				for (int i = 0; i < Dimensions; ++i)
+				{
+					newPoints[i] = this[i] / vector[i];
+				}
+				return new MathVector(newPoints);
+			}
+		}
+
+		public double CalcDistance(IMathVector vector)
+        {
+			if (vector.Dimensions != this.Dimensions)
+			{
+				return 0;
+			}
+			else
+			{
+				double[] newPoints = new double[Dimensions];
+				for (int i = 0; i < Dimensions; ++i)
+				{
+					newPoints[i] = this[i] - vector[i];
+				}
+				double res = new MathVector(newPoints).Length;
+				return res;
+			}
+        }
+
+		/* Перегрузка операторов */
+
+		public static IMathVector operator +(MathVector vector, double k)
+		{
+			return vector.SumNumber(k);
+		}
+
+		public static IMathVector operator +(MathVector vector, MathVector vector2)
+		{
+			return vector.Sum(vector2);
+		}
+
+		public static IMathVector operator -(MathVector vector, double k)
+		{
+			return vector.SumNumber(-k);
+		}
+
+		public static IMathVector operator -(MathVector vector, MathVector vector2)
+		{
+			return vector.Sum(vector2.MultiplyNumber(-1));
+		}
+
+		public static IMathVector operator *(MathVector vector, double k)
+		{
+			return vector.MultiplyNumber(k);
+		}
+
+		public static IMathVector operator *(MathVector vector, MathVector vector2)
+		{
+			return vector.Multiply(vector2);
+		}
+
+		public static IMathVector operator /(MathVector vector, double k)
+		{
+			return vector.Divide(k);
+		}
+
+		public static IMathVector operator /(MathVector vector, MathVector vector2)
+		{
+			return vector.Divide(vector2);
+		}
+
+		public static double operator %(MathVector vector, MathVector vector2)
+		{
+			return vector.ScalarMultiply(vector2);
+		}
+
+		public override string ToString()
+		{
+			string res = $"Вектор\nКоличество точек: {Dimensions}\nТочки:";
+			foreach (double point in this) // Работа с IEnumerator
+            {
+				res += " " + point.ToString();
+            }
+			res += $"\nДлина вектора: {Length}\n";
+			return res;
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return points.GetEnumerator();
+		}
+	}
+}
---
+++ VectorDemo/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using CSharp_LinearAlgebra;
+
+namespace VectorDemo
+{
+    class Program
+    {
+        private static MathVector ReadVector()
+        {
+            Console.Write("Введите точки: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string[] points = Console.ReadLine().Split();
+            Console.ResetColor();
+            double[] newPoints = new double[points.Length];
+            for (int i = 0; i < points.Length; ++i)
+            {
+                newPoints[i] = Convert.ToDouble(points[i]);
+            }
+            Console.WriteLine($"Вектор с количеством точек {points.Length} сохранен!\n");
+            return new MathVector(newPoints);
+        }
+
+        private static void WriteMenu()
+        {
+            string text = "1. Добавить вектор\n2. Показать всё\n3. Суммировать\n";
+            text += "4. Вычесть\n5. Умножить вектор\n6. Делить\n7. Дистанция между векторами\n";
+            text += "0. Выход\n----------\nВыбор: ";
+            Console.Write(text);
+        }
+
+        private static string ReadSelect()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string select = Console.ReadLine();
+            Console.ResetColor();
+            return select;
+        }
+
+        private static int ReadNumberOrVector()
+        {
+            Console.Write("1. Работать с числом\n2. Работать с другим вектором\nВыбор: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string sel = Console.ReadLine();
+            Console.ResetColor();
+            return Convert.ToInt32(sel);
+        }
+
+        private static int SelectIdVector()
+        {
+            Console.Write("Введите ID вектора: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int idCurrent = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            return idCurrent - 1;
+        }
+
+        private static int ReadNumber()
+        {
+            Console.Write("Введите число: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.ResetColor();
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Программа \"Математические вектора\"\n");
+            Console.ResetColor();
+            // double[] points = {1, 4, 4};
+            // MathVector vector1 = new MathVector(points);
+            MathVector[] vectors = new MathVector[50];
+            int count = 0;
+            string select = "";
+            int sel, idCurrent, idSecond;
+            while (select != "0")
+            {
+                WriteMenu();
+                select = ReadSelect();
+                switch (select)
+                {
+                    case "1":
+                        vectors[count++] = ReadVector();
+                        break;
+                    case "2":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        for (int i = 0; i < count; ++i)
+                        {
+                            Console.WriteLine($"{i+1}. " + vectors[i]);
+                        }
+                        Console.ResetColor();
+                        break;
+                    case "3":
+                        sel = ReadNumberOrVector();
+                        idCurrent = SelectIdVector();
+                        if (sel == 1)
+                        {
+                            int k = ReadNumber();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] + k); // Взаимодействие с интерфейсом
+                        } else if (sel == 2)
+                        {
+                            idSecond = SelectIdVector();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] + vectors[idSecond]);
+                        }
+                        break;
+                    case "4":
+                        sel = ReadNumberOrVector();
+                        idCurrent = SelectIdVector();
+                        if (sel == 1)
+                        {
+                            int k = ReadNumber();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] - k); // Взаимодействие с интерфейсом
+                        }
+                        else if (sel == 2)
+                        {
+                            idSecond = SelectIdVector();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] - vectors[idSecond]);
+                        }
+                        break;
+                    case "5":
+                        sel = ReadNumberOrVector();
+                        idCurrent = SelectIdVector();
+                        if (sel == 1)
+                        {
+                            int k = ReadNumber();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] * k); // Взаимодействие с интерфейсом
+                        }
+                        else if (sel == 2)
+                        {
+                            idSecond = SelectIdVector();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] * vectors[idSecond]);
+                        }
+                        break;
+                    case "6":
+                        sel = ReadNumberOrVector();
+                        idCurrent = SelectIdVector();
+                        if (sel == 1)
+                        {
+                            int k = ReadNumber();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] / k); // Взаимодействие с интерфейсом
+                        }
+                        else if (sel == 2)
+                        {
+                            idSecond = SelectIdVector();
+                            vectors[idCurrent] = (MathVector)(vectors[idCurrent] / vectors[idSecond]);
+                        }
+                        break;
+                    case "7":
+                        idCurrent = SelectIdVector();
+                        idSecond = SelectIdVector();
+                        double dist = vectors[idCurrent].CalcDistance(vectors[idSecond]);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"Расстояние между вектором {idCurrent} и вектором {idSecond} равно {dist}");
+                        Console.ResetColor();
+                        break;
+                    default:
+                        select = "0";
+                        break;
+                }
+            }
+        }
+    }
+}
