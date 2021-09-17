137a45aa071923d0c88b4a091d584f36b715816e
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2021-09-17 10:53:27 +0300

    Добавление Enum Operations. Исправление мелких ошибок.

--- VectorDemo/Program.cs
+++ VectorDemo/Program.cs
@@ -6,6 +6,18 @@ namespace VectorDemo
 {
     class Program
     {
+        public enum Operations
+        {
+            exit = 0,
+            add = 1,
+            show = 2,
+            sum = 3,
+            minus = 4,
+            multiply = 5,
+            divide = 6,
+            distance = 7,
+        }
+
         private static MathVector ReadVector()
         {
             Console.Write("Введите точки: ");
@@ -29,12 +41,12 @@ namespace VectorDemo
             Console.Write(text);
         }
 
-        private static string ReadSelect()
+        private static Operations ReadSelect()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             string select = Console.ReadLine();
             Console.ResetColor();
-            return select;
+            return (Operations)Convert.ToInt32(select);
         }
 
         private static int ReadNumberOrVector()
@@ -73,18 +85,18 @@ namespace VectorDemo
             // MathVector vector1 = new MathVector(points);
             MathVector[] vectors = new MathVector[50];
             int count = 0;
-            string select = "";
+            Operations select = Operations.add;
             int sel, idCurrent, idSecond;
-            while (select != "0")
+            while (select != Operations.exit)
             {
                 WriteMenu();
                 select = ReadSelect();
                 switch (select)
                 {
-                    case "1":
+                    case Operations.add:
                         vectors[count++] = ReadVector();
                         break;
-                    case "2":
+                    case Operations.show:
                         Console.ForegroundColor = ConsoleColor.Blue;
                         for (int i = 0; i < count; ++i)
                         {
@@ -92,7 +104,7 @@ namespace VectorDemo
                         }
                         Console.ResetColor();
                         break;
-                    case "3":
+                    case Operations.sum:
                         sel = ReadNumberOrVector();
                         idCurrent = SelectIdVector();
                         if (sel == 1)
@@ -105,7 +117,7 @@ namespace VectorDemo
                             vectors[idCurrent] = (MathVector)(vectors[idCurrent] + vectors[idSecond]);
                         }
                         break;
-                    case "4":
+                    case Operations.minus:
                         sel = ReadNumberOrVector();
                         idCurrent = SelectIdVector();
                         if (sel == 1)
@@ -119,7 +131,7 @@ namespace VectorDemo
                             vectors[idCurrent] = (MathVector)(vectors[idCurrent] - vectors[idSecond]);
                         }
                         break;
-                    case "5":
+                    case Operations.multiply:
                         sel = ReadNumberOrVector();
                         idCurrent = SelectIdVector();
                         if (sel == 1)
@@ -129,13 +141,21 @@ namespace VectorDemo
                         }
                         else if (sel == 2)
                         {
+                            
                             idSecond = SelectIdVector();
+                            Console.Write("1. Покомпонентное умножение\n2. Скалярное умножение\nВыбор: ");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            sel = Convert.ToInt32(Console.ReadLine());
+                            Console.ResetColor();
                             vectors[idCurrent] = (MathVector)(vectors[idCurrent] * vectors[idSecond]);
                         }
                         break;
-                    case "6":
+                    case Operations.divide:
                         sel = ReadNumberOrVector();
                         idCurrent = SelectIdVector();
+                        if (idCurrent < 0 || idCurrent >= count){
+                            break;
+                        }
                         if (sel == 1)
                         {
                             int k = ReadNumber();
@@ -147,7 +167,7 @@ namespace VectorDemo
                             vectors[idCurrent] = (MathVector)(vectors[idCurrent] / vectors[idSecond]);
                         }
                         break;
-                    case "7":
+                    case Operations.distance:
                         idCurrent = SelectIdVector();
                         idSecond = SelectIdVector();
                         double dist = vectors[idCurrent].CalcDistance(vectors[idSecond]);
@@ -156,7 +176,7 @@ namespace VectorDemo
                         Console.ResetColor();
                         break;
                     default:
-                        select = "0";
+                        select = Operations.exit;
                         break;
                 }
             }
